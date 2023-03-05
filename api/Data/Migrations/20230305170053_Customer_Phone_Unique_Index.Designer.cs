7a8cda36ca6b70f3dd23d54af2a1276e2d68b280
vladimir.petukhov@inrix.com <Vladimir.Petukhov@inrix.com>
2023-03-05 19:01:24 +0200

    ADD: migration

--- api/Data/Migrations/20230305170053_Customer_Phone_Unique_Index.Designer.cs
+++ api/Data/Migrations/20230305170053_Customer_Phone_Unique_Index.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using api.Data;
 
@@ -10,9 +11,10 @@ using api.Data;
 namespace api.Migrations
 {
     [DbContext(typeof(PizzaDbContext))]
-    partial class PizzaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230305170053_Customer_Phone_Unique_Index")]
+    partial class Customer_Phone_Unique_Index
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
